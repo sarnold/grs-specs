d53604f06653028042c7c8ec85cb3ea47c248a6d
Anthony G. Basile <blueness@gentoo.org>
2015-07-04 18:39:32 -0400

    Switch from udev to eudev.

--- scripts/cycle.1.sh
+++ scripts/cycle.1.sh
@@ -4,6 +4,8 @@ source /etc/profile
 env-update
 emerge -b1q portage
 emerge -b1q =app-portage/grs-9999
+emerge --unmerge sys-fs/udev
+emerge -b1q sys-fs/eudev
 install-worldconf
 emerge -bevq --keep-going --with-bdeps=y @world
 clean-worldconf
