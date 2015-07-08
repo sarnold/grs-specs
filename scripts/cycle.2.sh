8ae1516a880db7b3b24ec0a9692c78d4f23dd0c4
Anthony G. Basile <blueness@gentoo.org>
2015-07-08 07:39:24 -0400

    scripts/cycle.2.sh: add stage2 build.

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 
-source /etc/profile
-env-update
-install-worldconf
-emerge -buvNDq --keep-going --with-bdeps=y @world
-clean-worldconf
-emerge -bq @preserved-rebuild
+/usr/portage/scripts/bootstrap.sh
