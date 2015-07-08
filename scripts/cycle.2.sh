ba4f6a6dc3a85c7f2fd067d25bef72abb7445062
Anthony G. Basile <blueness@gentoo.org>
2015-07-08 10:03:31 -0400

    scripts/cycle.3.sh: add stage3 build.

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
 /usr/portage/scripts/bootstrap.sh -q
+
+for d in info doc man zoneinfo; do
+    find /usr/share -type d -iname ${d} -exec rm -rf {} +
+done
---
+++ scripts/cycle.3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+emerge -bkNe1q @system
