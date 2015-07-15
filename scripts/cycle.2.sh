1dfcd4e63007e8b5ad3a78e840d3e5f707e8a4c7
Anthony G. Basile <blueness@gentoo.org>
2015-07-14 20:30:16 -0400

    scripts/cycle.2.sh: remove tarball file.

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -4,6 +4,10 @@ source /etc/profile
 env-update
 emerge -b1q portage
 emerge -b1q =app-portage/grs-9999
+
+# This is from the tarball.
+rm -rf /etc/portage/package.use
+
 install-worldconf
 emerge -bevq --keep-going --with-bdeps=y @world
 clean-worldconf
