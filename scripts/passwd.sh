6cd46f9f0cc8213abd1121e6594d7f67e7146b9f
Anthony G. Basile <blueness@gentoo.org>
2015-07-27 14:32:35 -0400

    Let useradd create the home directory from /etc/skel

--- scripts/passwd.sh
+++ scripts/passwd.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+# Set the root password to 'root'
 echo root:root | chpasswd
 
+# Create a user 'gentoo' and set
+# the password to 'gentoo'
 useradd -m gentoo
+echo gentoo:gentoo | chpasswd
+
+# Make user 'gentoo' a power user
 gpasswd -a gentoo disk
 gpasswd -a gentoo wheel
 gpasswd -a gentoo audio
@@ -15,10 +21,8 @@ gpasswd -a gentoo usb
 
 gpasswd -a portage wheel
 
-echo gentoo:gentoo | chpasswd
-
-chown -R gentoo:gentoo /home/gentoo
-
+# For the grsec groups.  Make sure these
+# match whats in kernel-cofig!
 groupadd -g 9995 graudit
 groupadd -g 9996 grslink
 groupadd -g 9997 grasock
