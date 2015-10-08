a2eb5cc9fd3d3d411a570d1e8288cd4c6443b4f1
Anthony G. Basile <blueness@gentoo.org>
2015-10-08 18:56:42 -0400

    scripts/cycle.2.sh: force cdrkit for cdrtools.

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -3,6 +3,8 @@
 source /etc/profile
 env-update
 emerge -b1q portage
+emerge -b1q app-cdr/cdrkit
+emerge -b1q virtual/cdrtools
 emerge -b1q app-portage/grs
 install-worldconf
 emerge -bevq --keep-going --with-bdeps=y @world
