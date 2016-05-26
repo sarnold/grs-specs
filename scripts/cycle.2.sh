1ec4c3a3fcd5c7e5448aacc44675623a4ef25196
Anthony G. Basile <blueness@gentoo.org>
2016-05-26 13:31:50 -0400

    scripts/cycle.2.sh: sys-fs/eudev is now the default provider

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -4,8 +4,6 @@ source /etc/profile
 env-update
 emerge -b1q portage
 emerge -b1q app-portage/grs
-emerge --unmerge sys-fs/udev
-emerge -b1q sys-fs/eudev
 install-worldconf
 emerge -bevq --keep-going --verbose-conflicts --with-bdeps=y @world
 clean-worldconf
