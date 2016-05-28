088832cb6b09cbecd9c58a5ed72f28405f531074
Anthony G. Basile <blueness@gentoo.org>
2016-05-28 11:25:16 -0400

    scrips/cycle.1.sh: ensure uclibc is upgraded

--- scripts/cycle.1.sh
+++ scripts/cycle.1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
-# This comes from the stage3.
-rm -f /etc/portage/package.use/uclibc
-
 source /etc/profile
 env-update
+
+# This comes from the stage3, but we might
+# be building a different version of uclibc.
+rm -f /etc/portage/package.use/uclibc
+rm -f /etc/portage/savedconfig/sys-libs/uclibc
+emerge -b1q uclibc
+
 emerge -b1q binutils
 emerge -b1q gcc
 emerge -b1q uclibc
