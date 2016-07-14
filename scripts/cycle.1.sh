741e832ee8c92ab6efc2827167a7ce18c66e9154
Anthony G. Basile <blueness@gentoo.org>
2016-07-14 10:49:20 -0400

    Switch to uclibc-ng stages

--- scripts/cycle.1.sh
+++ scripts/cycle.1.sh
@@ -3,13 +3,7 @@
 source /etc/profile
 env-update
 
-# This comes from the stage3, but we might
-# be building a different version of uclibc.
-rm -f /etc/portage/package.use/uclibc
-rm -f /etc/portage/savedconfig/sys-libs/uclibc
-emerge -b1q uclibc
-
 emerge -b1q binutils
 emerge -b1q gcc
-emerge -b1q uclibc
+emerge -b1q uclibc-ng
 emerge -b1q argp-standalone
