5992646aef252dcb7edb05cf750adcf96d7c6286
Steve Arnold <nerdboy@gentoo.org>
2018-03-01 20:11:47 -0800

    mk-etc-issue.start: remove crufty script and do it the right way
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- scripts/initrc.sh
+++ scripts/initrc.sh
@@ -4,9 +4,10 @@
 ln -sf /etc/init.d/net.lo /etc/init.d/net.eth0
 touch /etc/udev/rules.d/80-net-name-slot.rules
 
-# set perms for portage
-chown portage:portage /usr/portage
-chmod g+s /usr/portage
+DATE=$(date +%Y%m%d)
+echo > /etc/issue
+echo "Gentoo GRS amd64 ${DATE} IP: \4{eth0}" >> /etc/issue
+echo >> /etc/issue
 
 # workaround for musl and ld.so
 touch /etc/ld.so.cache
@@ -21,6 +22,5 @@ rc-update add sshd default
 rc-update add xdm default
 rc-update add avahi-daemon default
 rc-update add dbus default
-rc-update add samba default
 rc-update add sinfod default
 rc-update add syslog-ng default
