35647202984c891aa4ec025b057b07d5af4b3853
Steve Arnold <nerdboy@gentoo.org>
2018-03-13 09:58:13 -0700

    scripts/cycle.2.sh: keywords workaround for grs and argp-standalone
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -3,8 +3,8 @@
 source /etc/profile
 env-update
 emerge -b1q portage
-emerge -b1q argp-standalone
-emerge -b1q app-portage/grs
+ACCEPT_KEYWORDS="~arm" emerge -b1q argp-standalone
+ACCEPT_KEYWORDS="~arm" emerge -b1q app-portage/grs
 
 install-worldconf
 
