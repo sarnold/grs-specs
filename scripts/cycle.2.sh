93157c9346db55d2a3b18a175555292e8789a2b6
Steve Arnold <nerdboy@gentoo.org>
2018-03-16 22:07:33 -0700

    scripts/cycle.2.sh: cleanup for new stage3, slight layman.cfg update
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- scripts/cycle.2.sh
+++ scripts/cycle.2.sh
@@ -2,6 +2,7 @@
 
 source /etc/profile
 env-update
+
 emerge -b1q portage
 ACCEPT_KEYWORDS="~arm" emerge -b1q argp-standalone
 ACCEPT_KEYWORDS="**" emerge -b1q app-portage/grs
@@ -9,15 +10,7 @@ ACCEPT_KEYWORDS="**" emerge -b1q app-portage/grs
 install-worldconf
 
 emerge -b1q binutils
-binutils-config 2
-emerge -qC binutils:2.28.1
-source /etc/profile && env-update
-
 emerge -b1q gcc
-gcc-config 2
-emerge -qC gcc:5.4.0
-source /etc/profile && env-update
-
 emerge -b1q musl
 
 emerge -bevq --keep-going --verbose-conflicts --with-bdeps=y @world
