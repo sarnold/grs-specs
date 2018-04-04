360e88d63d5de996c0ac0bc79e78f0a1b0995246
Steve Arnold <nerdboy@gentoo.org>
2018-04-04 00:15:03 -0700

    core,scripts: post-build fixups and cleanup
    * jigger etnaviv video support (more or less default)
    * move xorg,conf file to directory
    * add consolkit and ntp-client to rc-update
    * change hostname to most eating-est penguin
    
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- scripts/initrc.sh
+++ scripts/initrc.sh
@@ -16,6 +16,8 @@ mkdir -p /etc/ld.so.conf.d
 
 rc-update add alsasound default
 rc-update add cronie default
+rc-update add consolekit default
+rc-update add net.lo boot
 rc-update add net.eth0 default
 rc-update add postfix default
 rc-update add sshd default
@@ -24,3 +26,4 @@ rc-update add avahi-daemon default
 rc-update add dbus default
 rc-update add sinfod default
 rc-update add metalog default
+rc-update add ntp-client default
